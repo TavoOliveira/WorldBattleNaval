03f8b62b3d857e3fb4c65158468ca38cd646bd7d
tavobatista <otavio.batista@bringit.com.br>
2026-04-11 09:02:35 -0300

    Reformat InputManager fields and properties

--- WorldBattleNaval/InputManager.cs
+++ WorldBattleNaval/InputManager.cs
@@ -5,38 +5,48 @@ namespace WorldBattleNaval;
 
 public static class InputManager
 {
-    private static MouseState _current;
-    private static MouseState _previous;
-    private static KeyboardState _currentKeys;
-    private static KeyboardState _previousKeys;
+    private static MouseState current;
+    private static MouseState previous;
+    private static KeyboardState currentKeys;
+    private static KeyboardState previousKeys;
 
-    public static Point MousePosition => new(_current.X, _current.Y);
+    public static Point MousePosition => new(current.X, current.Y);
 
     // Mouse - held down
-    public static bool IsLeftDown    => _current.LeftButton   == ButtonState.Pressed;
-    public static bool IsRightDown   => _current.RightButton  == ButtonState.Pressed;
-    public static bool IsMiddleDown  => _current.MiddleButton == ButtonState.Pressed;
+    public static bool IsLeftDown => current.LeftButton == ButtonState.Pressed;
+    public static bool IsRightDown => current.RightButton == ButtonState.Pressed;
+    public static bool IsMiddleDown => current.MiddleButton == ButtonState.Pressed;
 
     // Mouse - just pressed this frame
-    public static bool IsLeftPressed   => _current.LeftButton   == ButtonState.Pressed  && _previous.LeftButton   == ButtonState.Released;
-    public static bool IsRightPressed  => _current.RightButton  == ButtonState.Pressed  && _previous.RightButton  == ButtonState.Released;
-    public static bool IsMiddlePressed => _current.MiddleButton == ButtonState.Pressed  && _previous.MiddleButton == ButtonState.Released;
+    public static bool IsLeftPressed =>
+        current.LeftButton == ButtonState.Pressed && previous.LeftButton == ButtonState.Released;
+
+    public static bool IsRightPressed =>
+        current.RightButton == ButtonState.Pressed && previous.RightButton == ButtonState.Released;
+
+    public static bool IsMiddlePressed =>
+        current.MiddleButton == ButtonState.Pressed && previous.MiddleButton == ButtonState.Released;
 
     // Mouse - just released this frame (click)
-    public static bool IsLeftClicked   => _current.LeftButton   == ButtonState.Released && _previous.LeftButton   == ButtonState.Pressed;
-    public static bool IsRightClicked  => _current.RightButton  == ButtonState.Released && _previous.RightButton  == ButtonState.Pressed;
-    public static bool IsMiddleClicked => _current.MiddleButton == ButtonState.Released && _previous.MiddleButton == ButtonState.Pressed;
+    public static bool IsLeftClicked =>
+        current.LeftButton == ButtonState.Released && previous.LeftButton == ButtonState.Pressed;
+
+    public static bool IsRightClicked =>
+        current.RightButton == ButtonState.Released && previous.RightButton == ButtonState.Pressed;
+
+    public static bool IsMiddleClicked =>
+        current.MiddleButton == ButtonState.Released && previous.MiddleButton == ButtonState.Pressed;
 
     // Keyboard
-    public static bool IsKeyDown(Keys key)     => _currentKeys.IsKeyDown(key);
-    public static bool IsKeyPressed(Keys key)  => _currentKeys.IsKeyDown(key) && !_previousKeys.IsKeyDown(key);
-    public static bool IsKeyReleased(Keys key) => !_currentKeys.IsKeyDown(key) && _previousKeys.IsKeyDown(key);
+    public static bool IsKeyDown(Keys key) => currentKeys.IsKeyDown(key);
+    public static bool IsKeyPressed(Keys key) => currentKeys.IsKeyDown(key) && !previousKeys.IsKeyDown(key);
+    public static bool IsKeyReleased(Keys key) => !currentKeys.IsKeyDown(key) && previousKeys.IsKeyDown(key);
 
     public static void Update()
     {
-        _previous     = _current;
-        _previousKeys = _currentKeys;
-        _current      = Mouse.GetState();
-        _currentKeys  = Keyboard.GetState();
+        previous = current;
+        previousKeys = currentKeys;
+        current = Mouse.GetState();
+        currentKeys = Keyboard.GetState();
     }
 }
